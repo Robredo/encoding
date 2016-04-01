5124099f5de1448bc4198195e1462a106c933330
Roberto Plancarte <robertoplancarte@gmail.com>
2016-04-01 08:58:57 -0600

    first commit

---
+++ EncodingClass.java
@@ -0,0 +1,35 @@
+public class EncodingClass {
+  private static byte    byte_value = 101;
+  private static short  short_value = 5;
+  private static int      int_value = 100000001;
+  private static long    long_value = 123456787654321L;
+
+  final static int byte_size = Byte.SIZE / Byte.SIZE;
+  final static int int_size = Integer.SIZE / Byte.SIZE;
+  final static int short_size = Short.SIZE / Byte.SIZE;
+  final static int long_size = Long.SIZE / Byte.SIZE;
+
+  private final static int mask = 0xFF;
+
+  public static void printByteArray(byte[] val){
+    StringBuilder str = new StringBuilder();
+    for(byte b : val){
+      str.append(b & mask).append(" ");
+    }
+    System.out.println(str.toString());
+  }
+
+  // Falta hacer un loop para 
+  public static byte[] encodeBigEndian(long val, int size){
+    byte[] ret = new byte[size];
+    ret[0] = (byte) val ;
+    return ret;
+  } 
+
+  public static void main(String[] args){
+    System.out.println("byte:" + byte_size + " short:" + 
+        short_size + " int:" + int_size + " long:" + long_size);
+    byte[] byte10 = encodeBigEndian(255, byte_size);
+    printByteArray(byte10); 
+  }
+}
